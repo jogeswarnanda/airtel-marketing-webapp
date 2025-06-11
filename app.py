30ad67ea404056732096b9f2a06d7a96e53ce978
jogeswarnanda <jogeswarnanda@gmail.com>
2025-06-12 01:00:22 +0530

    test

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
-def hello_world():
-    return "Hello, Joges !"
+def hello_airtel():
+    return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
