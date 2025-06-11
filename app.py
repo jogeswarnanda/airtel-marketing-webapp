ba451fd9e896794d9c029330b3b50bdeb30c5b0b
jogeswarnanda <jogeswarnanda@gmail.com>
2025-06-11 23:10:31 +0530

    hello world

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "Hello, Joges !"
+
+print(__name__)
+if __name__ == "__main__":
+    print("I am in if")
+    app.run(host ='0.0.0.0',port=8080, debug=True)
+    
