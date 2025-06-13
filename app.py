dc0fec300f0948a5db727bbf52ae38927cc0b98c
jogeswarnanda <jogeswarnanda@gmail.com>
2025-06-14 01:19:04 +0530

    true

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 def hello_airtel():
     return render_template('home.html')
 
+@app.route("/signup/")
+def signup_page():
+  print("Control signup  here.....")
+  return render_template("signup.html")
+
 print(__name__)
 if __name__ == "__main__":
     print("I am in if")
