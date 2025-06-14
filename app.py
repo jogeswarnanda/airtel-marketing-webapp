d2597fa22de2c95c596544eae65b04e0ee042eae
jogeswarnanda <jogeswarnanda@gmail.com>
2025-06-14 22:19:30 +0530

    updated

--- app.py
+++ app.py
@@ -1,16 +1,255 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, flash, url_for,session
+from database import verify_login,add_user_to_db,fetch_upload_data,upload_data,update_user_to_db
+import pandas as pd
+import os
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from datetime import datetime
+
+db = SQLAlchemy() 
 app = Flask(__name__)
+
+app.secret_key = 'your_secret_key'
+app.config['SESSION_TYPE'] = 'filesystem'
+# File upload folder
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# Define the model matching Excel structure
+class UserUpload(db.Model):
+    __tablename__ = 'user_uploads'
+
+    upload_id = db.Column(db.String(50), primary_key=True)
+    upload_mobile = db.Column(db.String(10))
+    upload_name = db.Column(db.String(100))
+    upload_month = db.Column(db.String(10))
+    upload_role = db.Column(db.String(10))
+    upload_fwa = db.Column(db.Integer)
+    upload_jmnp = db.Column(db.Integer)
+    upload_mnp =db.Column(db.Integer)
+    upload_mdsso = db.Column(db.Integer)
+    upload_simbilling = db.Column(db.Integer)
+
+
 @app.route("/")
 def hello_airtel():
     return render_template('home.html')
 
-@app.route("/signup/")
+@app.route("/signup/",methods=['GET', 'POST'])
 def signup_page():
-  print("Control signup  here.....")
-  return render_template("signup.html")
+    if request.method == 'POST':
+        s_mobile = request.form['s_mobile']
+        s_name = request.form['s_name']
+        s_role = request.form['s_role']
+        admin_key = request.form['s_admin_key']
+        print(s_mobile)
+        print(s_name)
+        data = request.form
+        found_stat = 'N'
+        dbpwd = " "
+        name = " "
+        role = " "
+        amobile = " "
+       
+        if ( s_role == "Admin" ):
+            if ( admin_key !="admin@1234"):
+                flash("Admin key mandetory for Admin registration!", "waring")
+            found_stat,dbpwd,name,role,amobile = verify_login(s_mobile, data, found_stat,dbpwd,name,role,amobile)
+            if ( found_stat == "Y"):
+                print("found post db call")
+                flash("Mobile number Already exist!", "waring")
+            else:
+                add_user_to_db(data)
+                flash("Admin Created !", "success")
+        else:
+            found_stat,dbpwd,name,role,amobile = verify_login(s_mobile, data, found_stat,dbpwd,name,role,amobile)
+            print("found_stat:",found_stat)
+            print("name:",name)
+            print("role:",role)
+            if ( found_stat == "Y"):
+                print("found post db call")
+                flash("Mobile number Already exist!", "waring")
+            else:
+                add_user_to_db(data)
+                flash("User Created !", "success")
+                # flash("Form submitted successfully!", "success")
+                return redirect(url_for('signup_page'))
+    return render_template('signup.html')
+
+@app.route("/login/",methods=['GET', 'POST'])
+def login_page():
+    print("in login render")
+    if request.method == 'POST':
+        login_mobile = request.form['mobile']
+        login_pwd    = request.form['password1']
+        print(login_mobile)
+        print(login_pwd)
+        data = request.form
+        found_stat = 'N'
+        dbpwd  = " "
+        name   = " "
+        role   = " "
+        amobile = " "
+        found_stat,dbpwd,name,role,amobile = verify_login(login_mobile, data, found_stat,dbpwd,name,role,amobile)
+        if found_stat == 'Y':
+            if (login_pwd == dbpwd):
+                print("matched pwd @@@")
+                #session['mobile_session'] = login_mobile
+                #session['mobile_session'] = login_mobile
+                session['username'] = name   
+                username = session['username']
+                session['mobile'] = login_mobile
+                mobile = session['mobile']   
+                #session['username'] = role
+                #username = session['username']
+                 #return render_template("userdashboard.html",username=username)
+                if  ( role == "Admin" ):
+                    return admin_dashboard()
+                else:
+                    return user_dashboard()
+
+            else:
+                return render_template("login.html",
+                               error="Invalid username or password")
+        else:
+            print("IN ELSE1 ", found_stat)
+            return render_template("login.html",
+                             error="User not found. Please register")
+    else:
+        return render_template('login.html')
+    
+@app.route("/userdashboard/")
+def user_dashboard():
+    dash_name = session['username']
+    mobile = session['mobile'] 
+    print("in user dashboard")
+    print("seeess mob", session['mobile'])
+    columns, rows = fetch_upload_data(mobile)
+
+    #print("name", username)
+
+    return render_template('userdashboard.html',username=dash_name,columns=columns,rows=rows)
+
+
+@app.route("/admindashboard/")
+def admin_dashboard():
+    dash_name = session['username']
+    mobile = session['mobile'] 
+    print("in admin dashboard")
+    print("seeess mob", session['mobile'])
+    columns, rows = fetch_upload_data(mobile)
+    #print("name", username)
+    return render_template('admindashboard.html',username=dash_name)
+
+
+#Logout route
+@app.route("/logout/", methods=['GET', 'POST'])
+def logout_page():
+  print("LOGOUT")
+  session.pop('username', None)
+  session.clear()
+  return redirect(url_for('login_page'))
+  #return redirect(url_for('home'
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect('/')
+
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect('/')
+
+    if file and file.filename.endswith(('.xlsx', '.xls')):
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        df = pd.read_excel(filepath)
+
+        # Optional: Preview in console
+        print(df.head())
+
+        # Iterate and insert into DB
+        now = datetime.now()
+        current_year = now.year
+            
+        for _, row in df.iterrows():
+            print("excel rows ")
+            print(row['Mobile_Number'])
+            record = UserUpload(
+            
+            upload_mobile=row['Mobile_Number'],
+            upload_name=row['Full_Name'],
+            upload_month=row['Month'],
+            upload_role=row['Role'],
+            upload_fwa=row['LLM_FWA'],
+            upload_jmnp=row['LLM_JMNP'],
+            upload_mnp=row['LLM_MNP'],
+            upload_mdsso=row['LLM_MDSSO'],
+            upload_simbilling=row['LLM_Sim_Billing'],
+            upload_id= str(row['Mobile_Number']) + "_" + str(row['Month']) + "_" + str(current_year)
+            )
+            print("rec", record.upload_id)
+            print("rec", record.upload_mobile)
+            upload_data(record)
+
+        flash('Excel file uploaded and data saved to database!')
+        return redirect('/admindashboard')
+
+    else:
+        flash('Invalid file type. Please upload .xlsx or .xls')
+        return redirect('/admindashboard')
+    
+@app.route("/updateprofile/",methods=['GET', 'POST'])
+def update_profile():
+    prof_name = session['username']
+    prof_mobile = session['mobile'] 
+    print("in update profile")
+    print("seeess mob", session['mobile'])
+    print("seeess name", session['username'])
+    #fetch_update_user(mobile)
+    #print("name", username)
+    amobile = " "   
+    data = " "
+    found_stat = 'N'
+    name = " "
+    role = " "
+    dbpwd = " "
+    found_stat,dbpwd,prof_name,role,prof_amobile = verify_login(prof_mobile, data, found_stat,dbpwd,name,role,amobile)
+    print("prof a mob", prof_amobile)
+    print("nnn", name)
+
+    if request.method == 'POST':
+        p_name = request.form['p_name']
+        p_mobile = request.form['p_mobile']
+        p_pmobile = request.form['p_pmobile']
+        data = request.form
+        found_stat = 'N'
+        dbpwd = " "
+        name = " "
+        role = " "
+        amobile = " "
+       
+        found_stat,dbpwd,name,role,amobile = verify_login(prof_mobile, data, found_stat,dbpwd,name,role,amobile)
+        print("found_stat:",found_stat)
+        print("name:",name)
+        print("role:",role)
+        if ( found_stat == "Y"):    
+            update_user_to_db(data)
+            flash("Data Updated!", "success")
+                # flash("Form submitted successfully!", "success")
+            return redirect(url_for('update_profile'))
+            
+    return render_template('updateprofile.html',prof_name=prof_name,prof_mobile=prof_mobile,
+                           prof_amobile=prof_amobile)
+
 
 print(__name__)
 if __name__ == "__main__":
+    app.config['SQLALCHEMY_DATABASE_URI'] =  "mysql+pymysql://4SzmE32uEz5t8DV.root:LCwy8CL3iWqMpxRD@gateway01.ap-southeast-1.prod.aws.tidbcloud.com/test?charset=utf8mb4"
     print("I am in if")
     app.run(host ='0.0.0.0',port=8080, debug=True)
     
---
+++ database.py
@@ -0,0 +1,160 @@
+import sqlalchemy
+from sqlalchemy import create_engine, text
+print(sqlalchemy.__version__)
+
+db_connection_string =  "mysql+pymysql://4SzmE32uEz5t8DV.root:LCwy8CL3iWqMpxRD@gateway01.ap-southeast-1.prod.aws.tidbcloud.com/test?charset=utf8mb4"
+
+engine = create_engine(db_connection_string,connect_args={
+    "ssl" :{
+        "ca": "/etc/ssl/cert.pem"
+    }
+})
+
+with engine.connect() as conn:
+    query = "SELECT * from users"
+    result = conn.execute(text(query))
+
+result_all = result.mappings().all()
+result_dict = []
+for row in result_all:
+    result_dict.append(dict(row))
+print(result_dict)
+print(type(result_dict))
+
+def verify_login (signup_mobile,data,found_stat,dbpwd,name,role,amobile):
+  print("mobile no. entered by user:", signup_mobile)
+  dbpwd = " "
+  name  = " "
+  role  = " "
+  amobile = " "
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM users where user_mobile = :usermobile"),
+           {"usermobile" : signup_mobile});
+    rows = result.all()
+    print(type(rows))
+    print("rows_new@@::", rows)
+    #print(rows[0][4])
+    if len(rows) == 0:
+      found_stat = "N"
+      print("set found sta N:")
+      return found_stat,dbpwd,name,role,amobile
+    else:
+      print("set found stat YY")
+      found_stat = "Y"
+      dbpwd= rows[0][2]
+      name = rows[0][1]
+      role = rows[0][3]
+      amobile = rows[0][4]
+      print("pwd", dbpwd)
+      return found_stat,dbpwd,name,role,amobile
+
+def add_user_to_db(data):
+  with engine.connect() as conn:
+    result1 = conn.execute(text("INSERT INTO users (user_mobile,user_name,user_password,user_role,user_alt_number) VALUES(:u_mobile,:u_name,:u_password,:u_role,:u_alt_number)"),
+    {"u_mobile": data.get("s_mobile"), "u_name" : data.get("s_name"),"u_password" : data.get("s_password"),"u_role" : data.get("s_role",),"u_alt_number" : data.get("s_alt_number")});
+    conn.commit()
+
+def fetch_upload_data (s_mobile):
+  print('IN user table')
+  f_mobile = s_mobile
+  list_all= []
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM user_uploads where upload_mobile = :f_mobile"), {"f_mobile" : f_mobile});
+    columns = ["Month","MNP","FWA","JioMNP","MDSSO","Sim Billing"]
+    rows_all = result.fetchall()
+    print("rows_all", rows_all)
+    for row in rows_all:
+      rows_s = (row.upload_month,row.upload_mnp,row.upload_fwa,row.upload_jmnp,row.upload_mdsso,row.upload_simbilling)
+      list_all.append(rows_s)
+  print("list_all", list_all)
+  return columns,list_all
+
+def upload_data(upload):
+  uploadid = upload.upload_id
+  print("upload db", upload)
+  print("upload db", upload.upload_id)
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM user_uploads where upload_id = :uploadid1"),
+           {"uploadid1" : uploadid});
+    rows = result.all()
+    if len(rows) == 0:
+      result1 = conn.execute(text("INSERT INTO user_uploads (upload_id,upload_mobile,upload_name,upload_month,upload_role,upload_fwa,upload_jmnp,upload_mnp,upload_mdsso,upload_simbilling) VALUES(:u_id,:u_mobile,:u_name,:u_month,:u_role,:u_fwa,:u_jmnp,:u_mnp,:u_mdsso,:u_simbilling)"),
+      {"u_id": upload.upload_id, "u_mobile" : upload.upload_mobile,"u_name" : upload.upload_name,"u_month" : upload.upload_month,"u_role" : upload.upload_role,"u_fwa" : upload.upload_fwa,"u_jmnp" : upload.upload_jmnp,"u_mnp" : upload.upload_mnp,"u_mdsso" : upload.upload_mdsso,"u_simbilling" : upload.upload_simbilling});
+      conn.commit()
+
+    else:
+      print("update ", upload.upload_id)
+      print("update ", upload.upload_mnp)
+
+      result1 = conn.execute(text("""
+      UPDATE user_uploads
+      SET
+        upload_mobile = :u_mobile,
+        upload_name = :u_name,
+        upload_month = :u_month,
+        upload_role = :u_role,
+        upload_fwa = :u_fwa,
+        upload_jmnp = :u_jmnp,
+        upload_mnp = :u_mnp,
+        upload_mdsso = :u_mdsso,
+        upload_simbilling = :u_simbilling
+        WHERE upload_id = :u_id
+        """),
+      {
+        "u_id": upload.upload_id,
+        "u_mobile": upload.upload_mobile,
+        "u_name": upload.upload_name,
+        "u_month": upload.upload_month,
+        "u_role": upload.upload_role,
+        "u_fwa": upload.upload_fwa,
+        "u_jmnp": upload.upload_jmnp,
+        "u_mnp": upload.upload_mnp,
+        "u_mdsso": upload.upload_mdsso,
+        "u_simbilling": upload.upload_simbilling
+      })
+      conn.commit()
+def update_user_to_db(data):
+  print("in update profile db", data)
+  with engine.connect() as conn:
+    result1 = conn.execute(text("""
+      UPDATE users
+      SET
+        user_name = :p_name,
+        user_alt_number = :p_pmobile      
+        WHERE user_mobile = :p_mobile                                          
+        """),
+      {
+        "p_name": data.get("p_name"),
+        "p_mobile": data.get("p_mobile"),
+        "p_pmobile": data.get("p_pmobile")
+      })
+    conn.commit()
+
+
+
+
+
+#result_dict1 = dict()
+#result_dict1 = dict(result_dict.key)
+#print(type(result_dict1))
+#print(result_dict1)
+
+
+
+#first_r = result_all[0]
+#first_d = (dict(first_r))
+#print(type(first_d))
+#print(first_d)
+#result_dict = list()
+#for row in result.all():
+#    print(row)
+#    result_dict.append(row)
+
+#print(type(result_dict))
+
+
+  
+
+
+
+
